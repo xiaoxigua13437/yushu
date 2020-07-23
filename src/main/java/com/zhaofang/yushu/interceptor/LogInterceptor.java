18b795d992e008bc9e646580cd80324e49389296
zhaofang <591821761@qq.com>
2020-07-23 15:25:14 +0800

    日志拦截器对所有请求这个链接的请求进行拦截

---
+++ src/main/java/com/zhaofang/yushu/interceptor/LogInterceptor.java
@@ -0,0 +1,50 @@
+package com.zhaofang.yushu.interceptor;
+
+
+import com.zhaofang.yushu.common.DateUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.NamedThreadLocal;
+import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.text.SimpleDateFormat;
+import java.util.Map;
+
+public abstract class LogInterceptor {
+
+    private Logger logger = LoggerFactory.getLogger(LogInterceptor.class);
+
+    private static final int MAX_SHOW_LENGTH = 30;
+    private static final ThreadLocal<Long> startTimeThreadLocal = new NamedThreadLocal("ThreadLocal StartTime");
+    public static final ThreadLocal<Map<String, Object>> executeResultThreadLocal = new NamedThreadLocal("Execute Result");
+
+    public LogInterceptor(){}
+
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if(this.logger.isDebugEnabled()) {
+            long beginTime = System.currentTimeMillis();
+            startTimeThreadLocal.set(Long.valueOf(beginTime));
+            this.logger.debug("开始计时:{}  URI: {} ", (new SimpleDateFormat("hh:mm:ss.SSS")).format(Long.valueOf(beginTime)), request.getRequestURI());
+        }
+
+        return true;
+    }
+
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+    }
+
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        this.saveLog(request, response);
+        if(this.logger.isDebugEnabled()) {
+            long beginTime = ((Long)startTimeThreadLocal.get()).longValue();
+            long endTime = System.currentTimeMillis();
+            this.logger.debug("计时结束：{}  耗时：{}  URI: {}  最大内存: {}m  已分配内存: {}m  已分配内存中的剩余空间: {}m", new Object[]{(new SimpleDateFormat("hh:mm:ss.SSS")).format(Long.valueOf(endTime)), DateUtil.formatDateTime(endTime - beginTime), request.getRequestURI(), Long.valueOf(Runtime.getRuntime().maxMemory() / 1024L / 1024L), Long.valueOf(Runtime.getRuntime().totalMemory() / 1024L / 1024L), Long.valueOf(Runtime.getRuntime().freeMemory() / 1024L / 1024L)});
+        }
+
+    }
+
+    public abstract void saveLog(HttpServletRequest var1, HttpServletResponse var2);
+
+
+}
---
+++ src/main/java/com/zhaofang/yushu/interceptor/WlanScopeLogInterceptor.java
@@ -0,0 +1,88 @@
+package com.zhaofang.yushu.interceptor;
+
+import com.zhaofang.yushu.common.LoggerUtils;
+import com.zhaofang.yushu.common.ParamUtil;
+import com.zhaofang.yushu.common.threadpool.ThreadUtil;
+import com.zhaofang.yushu.component.WebLogAspect;
+import com.zhaofang.yushu.dto.ApiAccessLog;
+import com.zhaofang.yushu.filter.BodyReaderFilter;
+import com.zhaofang.yushu.service.ApiAccessLogService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.NamedThreadLocal;
+import org.springframework.web.servlet.HandlerInterceptor;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 日志拦截器
+ * @author yushu
+ * @create 2020/07/21 15:05
+ */
+public class WlanScopeLogInterceptor extends LogInterceptor implements HandlerInterceptor {
+
+    private static final Logger logger = LoggerFactory.getLogger(WlanScopeLogInterceptor.class);
+
+    @Autowired
+    private ApiAccessLogService apiAccessLogService;
+
+    /**
+     * 设置日志信息
+     */
+    public static final ThreadLocal<ApiAccessLog> logInfoThreadLocal = new NamedThreadLocal(
+            "ThreadLocal  LogInfo");
+
+    @Override
+    public void saveLog(HttpServletRequest request, HttpServletResponse response) {
+        //获取请求,返回参数
+        String clientIp = LoggerUtils.getCliectIp(request);
+        String requestUserAgent = LoggerUtils.getClientUserAgent(request);
+        String url = request.getRequestURI();
+        String requestType = LoggerUtils.getRequestType(request);
+        String method = request.getMethod();
+        String requestParamters = ParamUtil.StringParam(BodyReaderFilter.getValueMyThreadLocal("requestParamters"));
+        Long requestTime = ParamUtil.LongParam(BodyReaderFilter.getValueMyThreadLocal("requestBeginTime"));
+        String responseCode = ParamUtil.StringParam(BodyReaderFilter.getValueMyThreadLocal("responseCode"));
+        String responseParamters = ParamUtil.StringParam(BodyReaderFilter.getValueMyThreadLocal("responseParamters"));
+
+        //将数据保存到线程变量
+        ApiAccessLog apiAccessLog = new ApiAccessLog();
+        apiAccessLog.setReqClientIp(clientIp);
+        apiAccessLog.setReqUserAgent(requestUserAgent);
+        apiAccessLog.setReqUri(url);
+        apiAccessLog.setReqType(requestType);
+        apiAccessLog.setReqMethod(method);
+        apiAccessLog.setReqData(requestParamters);
+        apiAccessLog.setReqTime(requestTime);
+        apiAccessLog.setRepTime(System.currentTimeMillis());
+        apiAccessLog.setRepData(responseParamters);
+        apiAccessLog.setRqpHttpStatusCode(responseCode);
+        if (requestTime != null) {
+            apiAccessLog.setCostTime((int) (System.currentTimeMillis() - requestTime));
+        }
+
+        //异步保存日志
+        ThreadUtil.excAsync(new SaveLogThread(apiAccessLog),true);
+
+        //销毁线程变量
+        logInfoThreadLocal.remove();
+
+
+    }
+
+
+
+    private class SaveLogThread implements Runnable {
+        private ApiAccessLog log;
+
+        public SaveLogThread(ApiAccessLog log) {
+            this.log = log;
+        }
+
+        @Override
+        public void run() {
+            apiAccessLogService.save(log);
+        }
+    }
+}

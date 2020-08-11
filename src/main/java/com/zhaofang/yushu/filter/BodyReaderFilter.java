54456e3c42376a61dcc7638eb6a5cf86584e8977
zhaofang <591821761@qq.com>
2020-08-11 14:33:11 +0800

    拦截请求,添加自定义逻辑(获取请求参数)重构后

--- src/main/java/com/zhaofang/yushu/filter/BodyReaderFilter.java
+++ src/main/java/com/zhaofang/yushu/filter/BodyReaderFilter.java
@@ -1,105 +1,115 @@
 package com.zhaofang.yushu.filter;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.NamedThreadLocal;
-
-import java.io.IOException;
-import java.security.Key;
-import java.util.HashMap;
-import java.util.Map;
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
-import javax.servlet.annotation.WebInitParam;
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
+ *
  * 拦截请求，添加自定义请求逻辑(获取请求参数)
  * 步骤：将取出来的字符串，再次转换成流，然后把它放入到新request 对象中
+ *
  * @author yushu
- * @create 2020-06-18 14:00
+ * @create 2020-08-11 11:10
  */
-//@WebFilter(filterName = "bodyReaderFilter",urlPatterns = "/*",initParams = {
-//        @WebInitParam(name = "exclusions"),
-//})
-public class BodyReaderFilter implements Filter{
+@WebFilter(filterName = "bodyReaderFilter",urlPatterns = "/*")
+public class BodyReaderFilter implements Filter {
 
-    private static ThreadLocal<Map<String,Object>> myThreadLocal = new NamedThreadLocal<>("My ThreadLocal");
+    private Logger logger = LoggerFactory.getLogger(BodyReaderFilter.class);
 
-    private final static Integer MAX_SHOW_LENGTH = 1000;
+    private static ThreadLocal<Map<String, Object>> myThreadLocal = new NamedThreadLocal<>("My ThreadLocal");
 
+    //最大请求报文数
+    private final static Integer MAX_SHOW_LENGTH = 1024;
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-
+        logger.info("初始化过滤器：{}", filterConfig.getFilterName());
     }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
-        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-        String contentType = request.getContentType();
-        String method = ((HttpServletRequest) request).getMethod();
+        HttpServletRequest httpServletRequest = null;
+        String requestType = null;
+        String method = null;
+
+      /*会出现运行时错误，需要用instanceOF判断
+        HttpServletRequest httpServletRequest = (HttpServletRequest)request;*/
+        //向下转型
+        if (request instanceof HttpServletRequest) {
+            httpServletRequest = (HttpServletRequest) request;
+            requestType = request.getContentType();
+            method = ((HttpServletRequest) request).getMethod();
+        }
         //设置请求时间
         BodyReaderFilter.addValueToMyThreadLocal("requestBeginTime",System.currentTimeMillis());
         //如果post请求使用application/json,application/x-www-form-urlencoded,text/xml请求方式，则用自定义请求拦截来获取请求参数
-        if (contentType != null && "post".equalsIgnoreCase(method) &&
-                (contentType.toLowerCase().startsWith("application/json") ||
-                 contentType.toLowerCase().startsWith("text/xml"))){
+        if (requestType != null && "post".equalsIgnoreCase(method) && (requestType.toLowerCase().startsWith("application/json"))){
 
             ServletRequest requestWrapper = new BodyReaderHttpServletRequestWrapper(httpServletRequest);
-            chain.doFilter(request,response);
+            chain.doFilter(requestWrapper,response);
 
         }else {
-            String paramters = formateParameter(request);
-            //获取请求参数
-            BodyReaderFilter.addValueToMyThreadLocal("requestParamters", paramters);
+            String parameters = FormatParameter(request);
+            //将获取的请求参数写入到线程变量里
+            BodyReaderFilter.addValueToMyThreadLocal("requestParameters",parameters);
             chain.doFilter(request, response);
         }
 
 
+
     }
-
-
 
     /**
      * 添加值到线程变量
-     * @param key
-     * @param value
+     *
+     * @param key 存储的key
+     * @param value 对应的值
      */
-    public static void addValueToMyThreadLocal(String key,Object value) {
+    public static void addValueToMyThreadLocal(String key,Object value){
         Map<String,Object> map = myThreadLocal.get();
         if (map == null){
             map = new HashMap<>();
         }
-
-        map.put(key,value);
+        map.put(key, value);
         myThreadLocal.set(map);
-
     }
 
     /**
      * 获取线程变量的值
-     * @param key
+     *
+     * @param key 存储的key
      * @return
      */
-    public static Object getValueMyThreadLocal(String key){
+    public static Object getValueToMyThreadLocal(String key){
         return myThreadLocal.get().get(key);
     }
 
-
     /**
-     * 销毁线程变量
+     * 销毁该线程变量
      */
     @Override
     public void destroy() {
         myThreadLocal.remove();
     }
 
+
     /**
      * 格式化参数
+     *
+     * @param request
+     * @return
      */
-    private String formateParameter(ServletRequest request) {
+    private String FormatParameter(ServletRequest request) {
         //日志参数格式化
         StringBuilder params = new StringBuilder("");
         Map<String, String[]> paramMap = request.getParameterMap();
@@ -116,7 +126,6 @@ public class BodyReaderFilter implements Filter{
             }
         }
         params.append("}");
-
         return params.toString();
     }
 

47043d73f4cbbc9a012cc098fc445f28e27f2101
loserfang <591821761@qq.com>
2020-08-16 12:12:51 +0800

    新增全局参数异常处理(用于@Validated @RequestBody配合使用)

--- src/main/java/com/zhaofang/yushu/exception/GlobalExceptionHandler.java
+++ src/main/java/com/zhaofang/yushu/exception/GlobalExceptionHandler.java
@@ -1,9 +1,16 @@
 package com.zhaofang.yushu.exception;
 
+import cn.hutool.core.util.StrUtil;
 import com.zhaofang.yushu.entity.CommonResult;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.ConstraintViolationException;
+import java.util.List;
 
 /**
  * 全局异常处理类
@@ -18,6 +25,39 @@ public class GlobalExceptionHandler {
             return CommonResult.failed(e.getErrorCode());
         }
         return CommonResult.failed(e.getMessage());
+    }
+
+
+    /**
+     *
+     * 全局参数异常处理
+     *
+     * @param exception
+     * @return
+     */
+    @ExceptionHandler(Exception.class)
+    public CommonResult handler(Exception exception) {
+
+        StringBuilder errMsg = new StringBuilder();
+        // 方法参数无效 异常
+        if(exception instanceof MethodArgumentNotValidException) {
+            BindingResult bindResult = ((MethodArgumentNotValidException) exception).getBindingResult();
+            List<FieldError> fieldErrorList = bindResult.getFieldErrors();
+            fieldErrorList.forEach(fieldErrors -> {
+                        FieldError fieldError = fieldErrors;
+                        if (StrUtil.isNotBlank(errMsg.toString())) {
+                            errMsg.append(",");
+                        }
+                        errMsg.append(fieldError.getDefaultMessage());
+                    }
+            );
+
+        }else if (exception instanceof ConstraintViolationException) {
+            // 约束冲突异常
+
+        }
+//        log.error("异常：",exception);
+        return CommonResult.failed(errMsg.toString());
     }
 
 

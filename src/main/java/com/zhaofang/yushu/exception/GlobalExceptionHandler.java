a852e59405c3186b8c42869548b80c58394bda1b
loserfang <591821761@qq.com>
2020-08-16 12:23:35 +0800

    自定义APP异常处理方法

--- src/main/java/com/zhaofang/yushu/exception/GlobalExceptionHandler.java
+++ src/main/java/com/zhaofang/yushu/exception/GlobalExceptionHandler.java
@@ -5,19 +5,28 @@ import com.zhaofang.yushu.entity.CommonResult;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import javax.validation.ConstraintViolationException;
+import java.util.List;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.validation.ConstraintViolationException;
-import java.util.List;
-
 /**
+ * @author yushu
+ * @create 2020-08-16
+ *
  * 全局异常处理类
  */
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+
+    /**
+     * 自定义API异常处理(凡是抛出ApiException 都会被捕捉到)
+     *
+     * @param e
+     * @return
+     */
     @ResponseBody
     @ExceptionHandler(value = ApiException.class)
     public CommonResult handle(ApiException e) {
@@ -32,6 +41,9 @@ public class GlobalExceptionHandler {
      *
      * 全局参数异常处理
      *
+     * 当使用了 @Validated + @RequestBody 注解但是没有在绑定的数据对象后面跟上 Errors 类型的参数声明的话，
+     * Spring MVC 框架会抛出 MethodArgumentNotValidException 异常。
+     * 当抛出 MethodArgumentNotValidException 异常,就会被相应的异常处理捕捉到理(有点类似于aop的意思)
      * @param exception
      * @return
      */
